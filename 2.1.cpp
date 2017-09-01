ac236dbeca49716f5c42eda6582d1b420f05378e
Abhaya0321 <31515214+Abhaya0321@users.noreply.github.com>
2017-09-01 22:27:45 +0530

    Add files via upload

---
+++ 2.1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+int cm, m;
+cout<<"Enter the distance in centimeters : ";
+cin>>cm;
+m = cm * 100;
+cout<<"The equivalent distance in meters is:";
+}
